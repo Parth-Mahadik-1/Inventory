5b5503b0faa55f5d1dac6f2659432fc0c7e6c65a
Parth-Mahadik-1 <parthmahadik44@gmail.com>
2024-12-01 20:40:00 +0530

    Add files via upload

---
+++ java learning/Variables_Datatypes/question3.java
@@ -0,0 +1,16 @@
+package Variables_Datatypes;
+import  java.util.Scanner;
+
+public class question3 {
+    public static void main(String[] args){
+        // program to greet user
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Name: ");
+        String name = sc.nextLine();
+
+        System.out.println("HELLO , "+ name + " Have a Good Day...:)");
+
+
+    }
+}
